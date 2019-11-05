c6f931e1ec439b0ab02d15961751f89b394a8238
Alx_is40 <alexian0620@gmail.com>
2019-11-05 16:44:19 +0100

    travail du 05/11/2019 journée

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ slider.addImage("../images/image_slider_1.jpg", "image de velo en station");
 slider.addImage("../images/image_slider_2.jpg", "deuxième image");
 slider.addImage("../images/image_slider_3.jpg", "troisième image");
 slider.addImage("../images/image_slider_4.jpg", "quatrième image");
+slider.shiftImage();
 slider.createButton();
-//slider.showImage();
-slider.changementImage();
+slider.animKeyboard();
+
--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,4 @@
+//console.log("test");
 //CRÉATION DE L'OBJET SLIDER
 class Slider {
     constructor(tagId){
@@ -5,25 +6,7 @@ class Slider {
         this.listImage = [];
         this.slider = document.getElementById(tagId);
         this.indexImage = 0;
-        
-    }
-
-//METHODE DE DEFILEMENT DES IMAGES.
-   changementImage(){
-       let that = this;
-       intervalId = setInterval(function(){
-        document.getElementById("image").innerHTML="";
-        that.indexImage++;
-        if (that.indexImage >= that.listImage.length){
-            that.indexImage = 0;
-        }
-        that.showImage();
-       }, 5000);
-   }
-
-//METHODE DE CRÉATION POUR LES IMAGES
-    addImage(pathImage, altImage){
-        this.listImage.push([pathImage, altImage]);
+        this.intervalId = null;
     }
 
 //CRÉATION D'UNE DIV QUI CONTIENDRA LES IMAGES.
@@ -33,41 +16,85 @@ class Slider {
         this.slider.appendChild(divImage);
     }
 
-//PERMET L'AFFICHAGE DES IMAGES
+//CRÉATION DES IMAGES
+    addImage(pathImage, altImage){
+        this.listImage.push([pathImage, altImage]);
+    }
+
+//AFFICHAGE DES IMAGES
     showImage(){
         let image = document.createElement("img");
         image.src = this.listImage[this.indexImage][0];
         image.alt = this.listImage[this.indexImage][1];
         image.style.width = "100%";
         document.getElementById("image").appendChild(image);
-        //setInterval(this.nextImage, 3000);
     }
-//CRÉATION DES BOUTONS NEXT ET PREV AINSI QUE LES EVENTS ASSOCIÉS
+
+//DEFILEMENT DES IMAGES EN AVANT
+    nextImage(){
+        document.getElementById("image").innerHTML="";
+        this.indexImage++;
+        if (this.indexImage >= this.listImage.length){
+            this.indexImage = 0;
+        }
+        this.showImage();
+    }     
+
+//DEFILEMENT DES IMAGES EN ARRIÈRE
+    prevImage(){
+        document.getElementById("image").innerHTML="";
+        this.indexImage--;
+        if (this.indexImage < 0){
+            this.indexImage = this.listImage.length-1;
+        }
+        this.showImage();
+    }        
+
+//CHANGEMENT AUTOMATIQUE DES IMAGES.
+   shiftImage(){
+       this.showImage();
+       let that = this;
+       this.intervalId = setInterval(function(){
+            that.nextImage();
+       }, 5000);
+       
+   }
+
+//CRÉATION DES BOUTONS NEXT, PREV ET STOP AINSI QUE LES EVENTS ASSOCIÉS
     createButton(){
         //Bouton suivant
         let buttonNext = document.createElement("button");
         buttonNext.textContent = "Suivant";
         this.slider.appendChild(buttonNext);
         buttonNext.addEventListener("click", function(){
-            document.getElementById("image").innerHTML="";
-            this.indexImage++;
-            if (this.indexImage >= this.listImage.length){
-                this.indexImage = 0;
-            }
-            this.showImage();
+            this.nextImage();
         }.bind(this));
 
-        //Boutton précedent
+        //Bouton précedent
         let buttonPrev = document.createElement("button");
         buttonPrev.textContent = "Précédent";
         this.slider.appendChild(buttonPrev);
         buttonPrev.addEventListener("click", function(){
-           document.getElementById("image").innerHTML="";
-            this.indexImage--;
-            if (this.indexImage < 0){
-               this.indexImage = this.listImage.length-1;
-           }
-            this.showImage();
+            this.prevImage();
         }.bind(this));
-    } 
+
+        //Bouton stop
+        let buttonStop = document.createElement("button");
+        buttonStop.textContent = "stop";
+        this.slider.appendChild(buttonStop);
+        buttonStop.addEventListener("click", function(){
+            clearInterval(this.intervalId);
+        }.bind(this));
+    }
+//CRÉATION DES EVENT AU CLICK SUR LES TOUCHES CLAVIERS.
+    animKeyboard(){
+        document.addEventListener("keypress", function(e){
+            //console.log(e.charCode)
+            if(e.keyCode === 43){
+                this.nextImage();
+            } else if(e.keyCode === 45){
+                this.prevImage();
+            }
+        }.bind(this));
+    }
 }
